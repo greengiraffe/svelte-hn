b69167371eccbc8a4d3f58257f6f74a0cac4da27
Valentin von Guttenberg <greengiraffe@posteo.de>
2020-10-14 17:27:19 +0200

    Add API documentation

--- src/api/index.js
+++ src/api/index.js
@@ -5,6 +5,12 @@ import { InvalidStoryTypeError } from "./errors"
 const BASE_URL = __ENV_VARS__.API_URL
 
 const API = {
+  /**
+   * Fetches a list of story items for a given page.
+   * 
+   * @param {string} storyType A valid story type (see api/storyTypes.js)
+   * @param {number} page The page number to fetch
+   */
   async stories(storyType, page = 1) {
     if (!validStoryType(storyType)) {
       return Promise.reject(new InvalidStoryTypeError(storyType))
@@ -15,10 +21,22 @@ const API = {
       return await res.json()
     }
   },
+
+  /**
+   * Fetch a story item
+   * 
+   * @param {number} id The story ID
+   */
   async item(id) {
     const res = await fetch(`${BASE_URL}item/${id}`)
     return await res.json()
   },
+
+  /**
+   * Fetch a user object
+   * 
+   * @param {number} id The user ID
+   */
   async user(id) {
     const res = await fetch(`${BASE_URL}user/${id}`)
     return await res.json()
